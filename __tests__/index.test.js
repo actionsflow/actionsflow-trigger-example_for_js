b3a881294f43469fac46cf65801062fad69021aa
Owen Young <theowenyoung@gmail.com>
2020-09-30 06:33:42 +0800

    feat: initial commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+const { resolve } = require("path");
+const Trigger = require("../index");
+const { getTriggerConstructorParams } = require("actionsflow-core");
+test("test trigger run", async () => {
+  const triggerConstructorParams = await getTriggerConstructorParams({
+    options: {
+      param1: "value",
+    },
+    name: "example_for_js",
+    cwd: resolve(__dirname, "fixtures"),
+    workflowPath: resolve(__dirname, "fixtures/workflows/workflow.yml"),
+  });
+  const trigger = new Trigger(triggerConstructorParams);
+  const result = await trigger.run();
+  expect(result.items.length).toBe(2);
+});
